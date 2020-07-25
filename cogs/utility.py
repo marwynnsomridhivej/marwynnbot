d7ecfd1eb9439fd267c16a476174522ae10a8b41
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-07-24 20:59:17 -0400

    - Added more toad

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,7 +1,7 @@
 import discord
 import json
 import os
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions, MissingPermissions, BotMissingPermissions
 
 
@@ -9,6 +9,7 @@ class Utility(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self.tasks = discord.ext.tasks
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -22,18 +23,28 @@ class Utility(commands.Cog):
             json.dump(values, f, indent=4)
 
     @commands.command(aliases=['used', 'usedcount'])
-    async def counter(self, ctx, commandName):
+    async def counter(self, ctx, commandName=None):
         await ctx.message.delete()
+
         with open('prefixes.json', 'r') as f:
             prefixes = json.load(f)
             serverPrefix = prefixes[str(ctx.guild.id)]
         with open('counters.json', 'r') as f:
-            value = json.load(f)
-            execCount = value[str(commandName)]
-        counterEmbed = discord.Embed(title=f"Command \"{commandName}\" Counter",
-                                     description=f"`{serverPrefix}{commandName}` was executed **{execCount}** "
-                                                 "times",
-                                     color=discord.Color.blue())
+            values = json.load(f)
+            keyList = values.items()
+            if commandName is not None:
+                execCount = values[str(commandName)]
+                counterEmbed = discord.Embed(title=f"Command \"{str.capitalize(commandName)}\" Counter",
+                                             description=f"`{serverPrefix}{commandName}` was executed **{execCount}** "
+                                                         "times",
+                                             color=discord.Color.blue())
+            else:
+                counterEmbed = discord.Embed(title="Command Counter",
+                                             color=discord.Color.blue())
+                for key, execCount in keyList:
+                    counterEmbed.add_field(name=f"Command: {str.capitalize(key)}",
+                                           value=f"Executed: **{execCount}** times")
+
         await ctx.channel.send(embed=counterEmbed)
         self.incrCounter('counter')
 
@@ -91,13 +102,44 @@ class Utility(commands.Cog):
 
     @commands.command(aliases=['ss', 'serverstats', 'serverstatistics'])
     @commands.bot_has_permissions(administrator=True)
-    @commands.has_permissions(manage_channels=True)
+    @commands.has_permissions(administrator=True)
     async def serverStats(self, ctx):
         await ctx.message.delete()
         client = self.client
         guild = client.get_guild(ctx.guild.id)
-        category = await guild.create_category(name="Server Stats")
+        totalMembers = guild.member_count
+        totalChannels = int(len(guild.channels))
+        totalText = int(len(guild.text_channels))
+        totalVoice = int(len(guild.voice_channels))
+        totalRole = int(len(guild.roles))
+        totalEmoji = int(len(guild.emojis))
+        overwrite = discord.PermissionOverwrite()
+        overwrite.connect = False
+        category = await guild.create_category(name="📊Server Stats📊")
+        await category.set_permissions(guild.default_role, overwrite=overwrite)
         await category.edit(position=0)
+        totalMembersVC = await category.create_voice_channel(name=f"👥Members: {totalMembers}",
+                                                             position=1,
+                                                             sync_permission=True)
+        totalChannelsVC = await category.create_voice_channel(name=f"📖Total Channels: {totalChannels}",
+                                                              position=2,
+                                                              sync_permission=True)
+        totalTextVC = await category.create_voice_channel(name=f"📑Text Channels: {totalText}",
+                                                          position=3,
+                                                          sync_permission=True)
+        totalVoiceVC = await category.create_voice_channel(name=f"🎧Voice Channels: {totalVoice}",
+                                                           position=4,
+                                                           sync_permission=True)
+        totalRoleVC = await category.create_voice_channel(name=f"📜Roles: {totalRole}",
+                                                          position=5,
+                                                          sync_permission=True)
+        totalEmojiVC = await category.create_voice_channel(name=f"😄Custom Emotes: {totalEmoji}",
+                                                           position=6,
+                                                           sync_permission=True)
+
+    @tasks.loop(minutes=10)
+    async def timer(self, ctx):
+
 
     @commands.command(aliases=['tz'])
     @commands.bot_has_permissions(administrator=True)
