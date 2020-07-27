c5b1faa78df1e4b34b06ba927b6a5841ca498702
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-07-27 08:38:14 -0400

    Added file_check function

--- globalcommands.py
+++ globalcommands.py
@@ -4,10 +4,14 @@ import json
 
 class GlobalCMDS:
 
+    def file_check(self, filenamepath: str, *init):
+        if not os.path.exists(filenamepath):
+            with open(filenamepath, 'w') as f:
+                for str in init:
+                    f.write(str)
+
     def incrCounter(self, cmdName):
-        if not os.path.exists('counters.json'):
-            with open('counters.json', 'w') as f:
-                f.write('{\n\n}')
+        self.file_check(self, "counters.json", '{\n\n}')
         with open('counters.json', 'r') as f:
             values = json.load(f)
             try:
