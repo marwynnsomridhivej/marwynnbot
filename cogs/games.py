980ce3edc4d0a7a8d9e0195a81642169c55b822c
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-07-31 13:35:17 -0400

    Added transfer + checking multiuser balance

--- cogs/games.py
+++ cogs/games.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 
 import discord
@@ -16,41 +17,73 @@ class Games(commands.Cog):
         print('Cog "games" has been loaded')
 
     @commands.command(aliases=['bal'])
-    async def balance(self, ctx, addArgs: typing.Optional[str] = None, toMember: typing.Optional[discord.Member] = None):
+    async def balance(self, ctx, member: commands.Greedy[discord.Member]=None):
         await ctx.message.delete()
-
         init = {'Balance': {}}
         gcmds.json_load(gcmds, 'balance.json', init)
         with open('balance.json', 'r') as f:
             file = json.load(f)
-            try:
-                file['Balance'][str(ctx.author.id)]
-            except KeyError:
-                file['Balance'][str(ctx.author.id)] = 1000
-                balance = 1000
-                f.close()
+            if member is None:
+                try:
+                    file['Balance'][str(ctx.author.id)]
+                except KeyError:
+                    file['Balance'][str(ctx.author.id)] = 1000
+                    balance = 1000
+                else:
+                    balance = file['Balance'][str(ctx.author.id)]
             else:
-                balance = file['Balance'][str(ctx.author.id)]
-                f.close()
+                description = ""
+                color = 0
+                for user in member:
+                    try:
+                        file['Balance'][str(user.id)]
+                    except KeyError:
+                        file['Balance'][str(user.id)] = 1000
+                        balance = 1000
+                    else:
+                        balance = file['Balance'][str(user.id)]
+                        if balance != 1:
+                            spelling = "credits"
+                        elif balance == 1:
+                            spelling = 'credit'
+                        if balance == 0:
+                            color += 1
+                        description += f"{user.mention} has ```{balance} {spelling}```\n"
         with open('balance.json', 'w') as f:
             json.dump(file, f, indent=4)
             f.close()
-        if balance != 1:
-            spelling = "credits"
-        elif balance == 1:
-            spelling = 'credit'
-        if balance > 0:
-            color = discord.Color.blue()
-        else:
-            color = discord.Color.dark_red()
 
-        balanceEmbed = discord.Embed(title="Your Current Balance",
-                                     description=f"{ctx.author.mention}, your current balance is: ```{balance} {spelling}```",
-                                     color=color)
-        balanceEmbed.set_thumbnail(url="https://cdn.discordapp.com/attachments/734962101432615006/738390147514499163"
-                                       "/chips.png")
-        await ctx.channel.send(embed=balanceEmbed, delete_after=30)
-        gcmds.incrCounter(gcmds, ctx, 'balance')
+        if member is None:
+            if balance != 1:
+                spelling = "credits"
+            elif balance == 1:
+                spelling = 'credit'
+            if balance > 0:
+                color = discord.Color.blue()
+            else:
+                color = discord.Color.dark_red()
+
+            balanceEmbed = discord.Embed(title="Your Current Balance",
+                                         description=f"{ctx.author.mention}, your current balance is: ```{balance} {spelling}```",
+                                         color=color)
+            balanceEmbed.set_thumbnail(url="https://cdn.discordapp.com/attachments/734962101432615006/738390147514499163"
+                                           "/chips.png")
+            await ctx.channel.send(embed=balanceEmbed, delete_after=30)
+            gcmds.incrCounter(gcmds, ctx, 'balance')
+        else:
+            if color == len(member):
+                color = discord.Color.dark_red()
+            else:
+                color = discord.Color.blue()
+            balanceEmbed = discord.Embed(title="Balances",
+                                         description=description,
+                                         color=color)
+            balanceEmbed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/734962101432615006/738390147514499163"
+                    "/chips.png")
+            await ctx.channel.send(embed=balanceEmbed, delete_after=30)
+            gcmds.incrCounter(gcmds, ctx, 'balance')
+
 
     @commands.command(aliases=['gamestats', 'stats'])
     async def gameStats(self, ctx, gameName=None):
@@ -100,6 +133,127 @@ class Games(commands.Cog):
                                              value=value)
                 await ctx.channel.send(embed=statsEmbed)
                 gcmds.incrCounter(gcmds, ctx, 'gameStats')
+
+    @commands.command()
+    async def transfer(self, ctx, amount: int = None, member: commands.Greedy[discord.Member] = None):
+        await ctx.message.delete()
+
+        cmdExit = False
+        if amount is None:
+            errorEmbed = discord.Embed(title="No Amount Specified",
+                                       description=f"{ctx.author.mention}, you must specify a credit amount to transfer",
+                                       color=discord.Color.dark_red())
+            await ctx.channel.send(embed=errorEmbed, delete_after=10)
+            cmdExit = True
+        if member is None:
+            errorEmbed = discord.Embed(title="No User Specified",
+                                       description=f"{ctx.author.mention}, you must specify user to transfer credit to",
+                                       color=discord.Color.dark_red())
+            await ctx.channel.send(embed=errorEmbed, delete_after=10)
+            cmdExit = True
+        if cmdExit:
+            return
+
+        memberString = ""
+        userlist = [ctx.author.id]
+        for members in member:
+            memberString += f"{members.mention}, "
+            userlist.append(members.id)
+
+        init = {'Balance': {}}
+        gcmds.json_load(gcmds, 'balance.json', init)
+        with open('balance.json', 'r') as f:
+            file = json.load(f)
+            for user in userlist:
+                try:
+                    file['Balance'][str(user)]
+                except KeyError:
+                    file['Balance'][str(user)] = 1000
+                    balance = 1000
+                    f.close()
+                else:
+                    balance = file['Balance'][str(user)]
+                    f.close()
+        with open('balance.json', 'w') as f:
+            json.dump(file, f, indent=4)
+            f.close()
+        if (int(file['Balance'][str(ctx.author.id)])) < (amount * (int(len(userlist)) - 1)):
+            if amount != 1:
+                spell = "credits"
+            else:
+                spell = "credit"
+            errorEmbed = discord.Embed(title="Insufficient Funds",
+                                       description=f"{ctx.author.mention}, you cannot transfer more than you have\n"
+                                                   f"*Attempted to transfer* "
+                                                   f"```{(amount * (int(len(userlist)) - 1))} {spell}```, only have"
+                                                   f"```{int(file['Balance'][str(ctx.author.id)])} {spell}```",
+                                       color=discord.Color.dark_red())
+            await ctx.channel.send(embed=errorEmbed, delete_after=10)
+            return
+        else:
+            if amount != 1:
+                spell = "credits"
+            else:
+                spell = "credit"
+            confirmEmbed = discord.Embed(title="Credits Transfer",
+                                         description=f"{ctx.author.mention}, are you sure you want to transfer\n"
+                                                     f"```{amount} {spell}```\nto {memberString[:-2]}",
+                                         color=discord.Color.blue())
+            message = await ctx.channel.send(embed=confirmEmbed)
+            await message.add_reaction('✅')
+            await message.add_reaction('❌')
+
+            def check(reaction, user):
+                if ctx.author == user and str(reaction.emoji) == '✅':
+                    return True
+                elif ctx.author == user and str(reaction.emoji) == '❌':
+                    return True
+                else:
+                    return False
+
+            try:
+                choice = await commands.AutoShardedBot.wait_for(self.client, 'reaction_add', timeout=60.0, check=check)
+                for item in choice:
+                    if str(item) == '✅':
+                        choice = 'confirm'
+                        break
+                    if str(item) == '❌':
+                        choice = 'cancel'
+                        break
+                if choice == 'confirm':
+                    await message.clear_reactions()
+                    description = "Successfully transferred:\n"
+                    with open('balance.json', 'r') as f:
+                        file = json.load(f)
+                        for members in member:
+                            file['Balance'][str(members.id)] += amount
+                            file['Balance'][str(ctx.author.id)] -= amount
+                            description += f"```{amount}``` ➡ {members.mention}\n"
+                        with open('balance.json', 'w') as f:
+                            json.dump(file, f, indent=4)
+                    confirmEmbed = discord.Embed(title="Credits Transfer Successful",
+                                                 description=description,
+                                                 color=discord.Color.blue())
+                    await message.edit(embed=confirmEmbed)
+                    gcmds.incrCounter(gcmds, ctx, 'transfer')
+                    return
+                if choice == 'cancel':
+                    await message.clear_reactions()
+                    confirmEmbed = discord.Embed(title="Credits Transfer Cancelled",
+                                                 description=f"{ctx.author.mention} cancelled the transfer\n",
+                                                 color=discord.Color.dark_red())
+                    await message.edit(embed=confirmEmbed, delete_after=10)
+                    return
+            except asyncio.TimeoutError:
+                await message.clear_reactions()
+                canceled = discord.Embed(title="Confirmation Timeout",
+                                         description=f"{ctx.author.mention}, transfer cancelled due to inactivity",
+                                         color=discord.Color.dark_red())
+                canceled.set_thumbnail(url='https://cdn.discordapp.com/attachments/734962101432615006'
+                                           '/738083697726849034/nocap.jpg')
+                canceled.set_footer(text=f"{ctx.author.name} did not provide a valid reaction within 60 seconds")
+                await message.edit(embed=canceled, delete_after=10)
+                return
 
 
 def setup(client):
