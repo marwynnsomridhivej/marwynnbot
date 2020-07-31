ca964cd2844fb72069b4f00afe5737a50e2429e7
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-07-31 14:45:22 -0400

    Allowed to view other member's stats

--- cogs/games.py
+++ cogs/games.py
@@ -17,7 +17,7 @@ class Games(commands.Cog):
         print('Cog "games" has been loaded')
 
     @commands.command(aliases=['bal'])
-    async def balance(self, ctx, member: commands.Greedy[discord.Member]=None):
+    async def balance(self, ctx, member: commands.Greedy[discord.Member] = None):
         await ctx.message.delete()
         init = {'Balance': {}}
         gcmds.json_load(gcmds, 'balance.json', init)
@@ -66,8 +66,9 @@ class Games(commands.Cog):
             balanceEmbed = discord.Embed(title="Your Current Balance",
                                          description=f"{ctx.author.mention}, your current balance is: ```{balance} {spelling}```",
                                          color=color)
-            balanceEmbed.set_thumbnail(url="https://cdn.discordapp.com/attachments/734962101432615006/738390147514499163"
-                                           "/chips.png")
+            balanceEmbed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/734962101432615006/738390147514499163"
+                    "/chips.png")
             await ctx.channel.send(embed=balanceEmbed, delete_after=30)
             gcmds.incrCounter(gcmds, ctx, 'balance')
         else:
@@ -84,55 +85,78 @@ class Games(commands.Cog):
             await ctx.channel.send(embed=balanceEmbed, delete_after=30)
             gcmds.incrCounter(gcmds, ctx, 'balance')
 
-
     @commands.command(aliases=['gamestats', 'stats'])
-    async def gameStats(self, ctx, gameName=None):
+    async def gameStats(self, ctx, gameName: typing.Optional[str] = None,
+                        member: commands.Greedy[discord.Member] = None):
         await ctx.message.delete()
-        with open('gamestats.json', 'r') as f:
-            file = json.load(f)
-            if gameName is not None:
-                try:
-                    stats = file[str(gameName)][str(ctx.author.id)]
-                    statsEmbed = discord.Embed(title=f"Stats for {str(gameName).capitalize()}",
-                                               color=discord.Color.blue())
-                except KeyError:
-                    errorEmbed = discord.Embed(title="No Stats Available",
-                                               description=f"{ctx.author.mention}, you have no stats for this game",
-                                               color=discord.Color.dark_red())
-                    await ctx.channel.send(embed=errorEmbed, delete_after=5)
+        if gameName is not None:
+            if "@" in gameName:
+                userid = gameName[3:-1]
+                if member is None:
+                    member = [await commands.AutoShardedBot.fetch_user(self.client, user_id=userid)]
                 else:
-                    for item in stats:
-                        value = file[str(gameName)][str(ctx.author.id)][item]
-                        if value == 1:
-                            spell = "time"
+                    member.append(userid)
+                gameName = None
+        userlist = []
+        if member is None:
+            userlist.append(ctx.author.id)
+        else:
+            for user in member:
+                userlist.append(user.id)
+
+        for user in userlist:
+            person_name = await commands.AutoShardedBot.fetch_user(self.client, user_id=user)
+            with open('gamestats.json', 'r') as f:
+                file = json.load(f)
+                if gameName is not None:
+                    try:
+                        stats = file[str(gameName)][str(user)]
+                        statsEmbed = discord.Embed(title=f"{person_name.display_name}'s Stats for {str(gameName).capitalize()}",
+                                                   color=discord.Color.blue())
+                    except KeyError:
+                        errorEmbed = discord.Embed(title="No Stats Available",
+                                                   description=f"{ctx.author.mention}, you have no stats for this game",
+                                                   color=discord.Color.dark_red())
+                        await ctx.channel.send(embed=errorEmbed, delete_after=5)
+                    else:
+                        for item in stats:
+                            value = file[str(gameName)][str(user)][item]
+                            if item == "ratio":
+                                spell = ""
+                            else:
+                                if value == 1:
+                                    spell = "time"
+                                else:
+                                    spell = "times"
+                            statsEmbed.add_field(name=str(item).capitalize(),
+                                                 value=f"**{value}** {spell}",
+                                                 inline=False)
+                        await ctx.channel.send(embed=statsEmbed)
+                        gcmds.incrCounter(gcmds, ctx, 'gameStats')
+                else:
+                    statsEmbed = discord.Embed(title=f"{person_name.display_name}'s Stats for All Games",
+                                               color=discord.Color.blue())
+                    for game in file:
+                        try:
+                            user_data = file[str(game)][str(user)]
+                        except KeyError:
+                            statsEmbed.add_field(name=game,
+                                                 value="No Data Available")
                         else:
-                            spell = "times"
-                        statsEmbed.add_field(name=str(item).capitalize(),
-                                             value=f"**{value}** {spell}",
-                                             inline=False)
+                            value = ""
+                            for item in user_data:
+                                if item == "ratio":
+                                    spell = ""
+                                else:
+                                    if file[str(game)][str(user)][str(item)] == 1:
+                                        spell = "time"
+                                    else:
+                                        spell = "times"
+                                value += f"**{str(item).capitalize()}:** `{str(file[str(game)][str(user)][str(item)])}` {spell}\n"
+                            statsEmbed.add_field(name=game,
+                                                 value=value)
                     await ctx.channel.send(embed=statsEmbed)
                     gcmds.incrCounter(gcmds, ctx, 'gameStats')
-            else:
-                statsEmbed = discord.Embed(title=f"Stats for All Games",
-                                           color=discord.Color.blue())
-                for game in file:
-                    try:
-                        user_data = file[str(game)][str(ctx.author.id)]
-                    except KeyError:
-                        statsEmbed.add_field(name=game,
-                                             value="No Data Available")
-                    else:
-                        value = ""
-                        for item in user_data:
-                            if file[str(game)][str(ctx.author.id)][str(item)] == 1:
-                                spell = "time"
-                            else:
-                                spell = "times"
-                            value += f"**{str(item).capitalize()}:** `{str(file[str(game)][str(ctx.author.id)][str(item)])}` {spell}\n"
-                        statsEmbed.add_field(name=game,
-                                             value=value)
-                await ctx.channel.send(embed=statsEmbed)
-                gcmds.incrCounter(gcmds, ctx, 'gameStats')
 
     @commands.command()
     async def transfer(self, ctx, amount: int = None, member: commands.Greedy[discord.Member] = None):
