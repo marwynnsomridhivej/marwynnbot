7018e979c3ad50d38d6a8b31843a92b03ce0f231
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-08-12 17:36:31 -0400

    added check invkDelete, msgDelete, isGuild, prefix checking

--- globalcommands.py
+++ globalcommands.py
@@ -1,5 +1,6 @@
 import json
 import os
+import discord
 
 
 class GlobalCMDS:
@@ -10,28 +11,13 @@ class GlobalCMDS:
                 for str in init:
                     f.write(str)
 
-    def json_load(self, filenamepath, init):
-        if not os.path.exists(filenamepath):
-            with open(filenamepath, 'w') as f:
-                json.dump(init, f, indent=4)
-
     def incrCounter(self, ctx, cmdName):
+
         init = {'Server': {}, 'Global': {}}
 
         self.json_load(self, "./counters.json", init)
         with open('counters.json', 'r') as f:
             values = json.load(f)
-
-            try:
-                values['Server'][str(ctx.guild.id)]
-            except KeyError:
-                values['Server'][str(ctx.guild.id)] = {}
-            try:
-                values['Server'][str(ctx.guild.id)][str(cmdName)]
-            except KeyError:
-                values['Server'][str(ctx.guild.id)][str(cmdName)] = 1
-            else:
-                values['Server'][str(ctx.guild.id)][str(cmdName)] += 1
 
             try:
                 values['Global'][str(cmdName)]
@@ -40,10 +26,46 @@ class GlobalCMDS:
             else:
                 values['Global'][str(cmdName)] += 1
 
+            if not self.isGuild(self, ctx):
+                pass
+            else:
+                try:
+                    values['Server'][str(ctx.guild.id)]
+                except KeyError:
+                    values['Server'][str(ctx.guild.id)] = {}
+                try:
+                    values['Server'][str(ctx.guild.id)][str(cmdName)]
+                except KeyError:
+                    values['Server'][str(ctx.guild.id)][str(cmdName)] = 1
+                else:
+                    values['Server'][str(ctx.guild.id)][str(cmdName)] += 1
+
         with open('counters.json', 'w') as f:
             json.dump(values, f, indent=4)
 
+    async def invkDelete(self, ctx):
+        if isinstance(ctx.channel, discord.TextChannel):
+            await ctx.message.delete()
+
+    async def msgDelete(self, message):
+        if isinstance(message.channel, discord.TextChannel):
+            await message.delete()
+
+    def isGuild(self, ctx):
+        if isinstance(ctx.channel, discord.TextChannel):
+            return True
+        else:
+            return False
+
+    def json_load(self, filenamepath, init):
+        if not os.path.exists(filenamepath):
+            with open(filenamepath, 'w') as f:
+                json.dump(init, f, indent=4)
+
     def prefix(self, ctx):
+        if not self.isGuild(self, ctx):
+            return "m!"
+
         with open('prefixes.json', 'r') as f:
             prefixes = json.load(f)
 
