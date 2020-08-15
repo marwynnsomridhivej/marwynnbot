e66144176a16c6a8da0fdf9d1a86ad3d51c0d466
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-08-14 23:04:33 -0400

    added video class
    Signed-off-by: marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>

--- cogs/music.py
+++ cogs/music.py
@@ -4,7 +4,6 @@ import asyncio
 import youtube_dl
 import math
 import json
-from video import Video
 from globalcommands import GlobalCMDS as gcmds
 
 
@@ -74,6 +73,52 @@ class GuildState:
 
     def is_requester(self, user):
         return self.now_playing.requested_by == user
+
+YTDL_OPTS = {
+    "default_search": "ytsearch",
+    "format": "bestaudio/best",
+    "quiet": True,
+    "extract_flat": "in_playlist"
+}
+
+
+class Video:
+    """Class containing information about a particular video."""
+
+    def __init__(self, url_or_search, requested_by):
+        """Plays audio from (or searches for) a URL."""
+        with youtube_dl.YoutubeDL(YTDL_OPTS) as ydl:
+            video = self._get_info(url_or_search)
+            video_format = video["formats"][0]
+            self.stream_url = video_format["url"]
+            self.video_url = video["webpage_url"]
+            self.title = video["title"]
+            self.uploader = video["uploader"] if "uploader" in video else ""
+            self.thumbnail = video[
+                "thumbnail"] if "thumbnail" in video else None
+            self.requested_by = requested_by
+
+    def _get_info(self, video_url):
+        with youtube_dl.YoutubeDL(YTDL_OPTS) as ydl:
+            info = ydl.extract_info(video_url, download=False)
+            video = None
+            if "_type" in info and info["_type"] == "playlist":
+                return self._get_info(
+                    info["entries"][0]["url"])  # get info for first video
+            else:
+                video = info
+            return video
+
+    def get_embed(self):
+        """Makes an embed out of this Video's information."""
+        embed = discord.Embed(
+            title=self.title, description=self.uploader, url=self.video_url)
+        embed.set_footer(
+            text=f"Requested by {self.requested_by.name}",
+            icon_url=self.requested_by.avatar_url)
+        if self.thumbnail:
+            embed.set_thumbnail(url=self.thumbnail)
+        return embed
 
 
 class Music(commands.Cog):
