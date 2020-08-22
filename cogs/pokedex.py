14d911a876d9e0e2f53fa63bf7e31e2ce47292bf
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-08-22 15:49:45 -0400

    added move dex search
    Signed-off-by: marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>

--- cogs/pokedex.py
+++ cogs/pokedex.py
@@ -7,6 +7,9 @@ import pokepy
 from globalcommands import GlobalCMDS as gcmds
 
 poke_client = pokepy.V2Client(cache='in_disk', cache_location="./pokepy_cache")
+move_status_icon_urls = ["https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/e/ef/Physical.png?width=325",
+                         "https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/2/24/Special.png?width=325",
+                         "https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/d/d0/Status.png?width=325"]
 
 
 class Pokedex(commands.Cog):
@@ -61,9 +64,63 @@ class Pokedex(commands.Cog):
         weight = f"Weight: `{value.weight / 10} kg`"
         return pokemon_name, nat_dex_num, type, abilities, items, weight, height, base_xp
 
+    async def check_move(self, name) -> pokepy.api.rv2.MoveResource:
+        try:
+            return poke_client.get_move(name)
+        except Exception:
+            return None
+
+    async def get_move_entry(self, name):
+        value = await self.check_move(name)
+        if not value:
+            return None
+
+        move_name = f'Name: `{value.name.replace("-", " ").capitalize()}`'
+        if value.accuracy:
+            move_accuracy = f"Accuracy: `{value.accuracy}`"
+        else:
+            move_accuracy = f"Accuracy: `N/A`"
+        move_effect_entry = f"Description: `{value.effect_entries[0]['effect']}`"
+        move_type = f"Type: `{value.type.name.capitalize()}`"
+        move_damage_class = f"Move Class: `{value.damage_class.name.capitalize()}`"
+        if value.power:
+            move_power = f"Power: `{value.power}`"
+        else:
+            move_power = "Power: `N/A`"
+        move_pp = f"PP: `{value.pp}`"
+        move_max_pp = f"Max PP: `{math.ceil(float(value.pp) * 8.0 / 5.0)}`"
+        move_priority = f"Priority: `{value.priority}`"
+        move_target = f"Target: `{value.target.name.replace('-', ' ').capitalize()}`"
+        stat_changes = [(stat.stat.name, stat.change) for stat in value.stat_changes]
+        move_stat_changes = "User Stat Changes: "
+        if not stat_changes:
+            move_stat_changes += "`N/A`"
+        else:
+            for stat_name, stat_change_amount in stat_changes:
+                if stat_change_amount < 0:
+                    r_or_l = "Lowers"
+                else:
+                    r_or_l = "Raises"
+                if math.fabs(stat_change_amount) == 1.0:
+                    spell = "stage"
+                else:
+                    spell = "stages"
+                move_stat_changes += f"{r_or_l} the user's {stat_name} by {int(math.fabs(stat_change_amount))} {spell}\n"
+        return (move_name, move_effect_entry, move_type, move_damage_class, move_power, move_accuracy,
+                move_target, move_pp, move_max_pp, move_priority, move_stat_changes)
+
     async def get_dex_sprite(self, name):
         value = await self.check_pokemon(name)
         return value.sprites.front_default, value.sprites.front_shiny
+
+    async def get_move_status_icon(self, name):
+        value = await self.get_move_entry(name)
+        if "physical" in value[3].lower():
+            return move_status_icon_urls[0]
+        elif "special" in value[3].lower():
+            return move_status_icon_urls[1]
+        elif "status" in value[3].lower():
+            return move_status_icon_urls[2]
 
     @commands.group(aliases=['dex'])
     async def pokedex(self, ctx):
@@ -121,7 +178,20 @@ class Pokedex(commands.Cog):
 
     @pokedex.command(aliases=['-m', 'moves'])
     async def move(self, ctx, *, move_name: str):
-
+        move_name_sent = move_name.replace(" ", "-").lower()
+        value = await self.get_move_entry(move_name_sent)
+        if value:
+            embed = discord.Embed(title=move_name.capitalize(),
+                                  description=f"{ctx.author.mention}, here is {move_name.capitalize()}'s move entry\n\n",
+                                  color=discord.Color.blue())
+            embed.description += "\n".join(value)
+            embed.set_thumbnail(url=await self.get_move_status_icon(move_name_sent))
+            return await ctx.channel.send(embed=embed)
+        else:
+            invalid = discord.Embed(title="Invalid Move Name",
+                                    description = f"{ctx.author.mention}, `{move_name}` is not a valid move",
+                                    color=discord.Color.dark_red())
+            return await ctx.channel.send(embed=invalid, delete_after=5)
 
 
 def setup(client):
