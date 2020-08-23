44e20ca6c1181de9c20b80b36bc3e1e81a09113e
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-08-23 13:34:27 -0400

    added minecraft commands
    Signed-off-by: marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -13,6 +13,62 @@ class Minecraft(commands.Cog):
     async def on_ready(self):
         print(f'Cog "{self.qualified_name}" has been loaded')
 
+    async def get_server(self, host: str) -> MinecraftServer:
+        try:
+            return MinecraftServer.lookup(host)
+        except Exception:
+            return None
+
+    async def get_ping(self, ctx, host: str) -> discord.Message:
+        server = await self.get_server(host)
+        if not server:
+            return None
+
+        try:
+            latency = server.ping()
+        except Exception:
+            latency_str = "Could not reach server"
+        else:
+            latency_str = f"The server replied in {latency} ms"
+        embed = discord.Embed(title=f"Ping for {host}",
+                              description=latency_str,
+                              color=discord.Color.blue())
+        return await ctx.channel.send(embed=embed)
+
+    async def get_query(self, ctx, host: str) -> discord.Message:
+        server = await self.get_server(host)
+        if not server:
+            return None
+        try:
+            status = server.query()
+        except Exception:
+            embed = discord.Embed(title="Request Timed Out",
+                                  description=f"{ctx.author.mention}, your request was timed out. Please try again later",
+                                  color=discord.Color.dark_red())
+            return await ctx.channel.send(embed=embed, delete_after=10)
+        players_online = f"Players Online: `{status.players.online}`"
+        max_players = f"Max Players: `{status.players.max}`"
+        software = f"Software: `{status.software.brand.title()} {status.software.version}`"
+        if not status.software.plugins:
+            plugins = "Plugins: `N/A`"
+        else:
+            plugins = f"Plugins: `{'` `'.join(status.software.plugins)}"
+        motd = f"MOTD: `{status.motd}`"
+
+        items_list = [players_online, max_players, software, plugins, motd]
+
+        embed = discord.Embed(title=f"Details for {host}",
+                              description=f"{ctx.author.mention}, here are the details for the requested server\n\n",
+                              color=discord.Color.blue())
+        embed.description += "\n".join(items_list)
+        return await ctx.channel.send(embed=embed)
+
+    async def invalid(self, ctx, host: str) -> discord.Message:
+        embed = discord.Embed(title="Invalid Server",
+                              description=f"{ctx.author.mention}, no server exists for hostname {host}",
+                              color=discord.Color.dark_red())
+        return await ctx.channel.send(embed=embed, delete_after=10)
+
     @commands.group(aliases=['mc'])
     @commands.cooldown(1, 10, commands.BucketType.member)
     async def minecraft(self, ctx):
@@ -44,7 +100,16 @@ class Minecraft(commands.Cog):
         if not host:
             return await ctx.invoke(self.minecraft)
 
+        if not await self.get_ping(ctx, host):
+            return await self.invalid(ctx, host)
 
+    @minecraft.command(aliases=['-d', '-q', 'query'])
+    async def details(self, ctx, host: str = None):
+        if not host:
+            return await ctx.invoke(self.minecraft)
+
+        if not await self.get_query(ctx, host):
+            return await self.invalid(ctx, host)
 
 
 def setup(client):
