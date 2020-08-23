13831bdbfb5f78eb0cf428cceaf2e6f4d29d1c08
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-08-23 17:11:10 -0400

    refactoring creating reaction role panel
    Signed-off-by: marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>

--- cogs/reactions.py
+++ cogs/reactions.py
@@ -1,10 +1,11 @@
 import asyncio
-
+import re
 import discord
-import json
 from discord.ext import commands
 from globalcommands import GlobalCMDS as gcmds
 
+channel_tag_rx = re.compile(r'<#[0-9]{18}>')
+channel_id_rx = re.compile(r'[0-9]{18}')
 
 class Reactions(commands.Cog):
 
@@ -15,215 +16,143 @@ class Reactions(commands.Cog):
     async def on_ready(self):
         print(f'Cog "{self.qualified_name}" has been loaded')
 
+    async def check_panel(self, panel: discord.Message) -> discord.Message:
+        try:
+            return panel
+        except discord.NotFound:
+            return None
+
+    async def edit_panel(self, panel_embed: discord.Embed, panel: discord.Message,
+                         title: str = None, description: str = None) -> discord.Message:
+        if title:
+            panel_embed.title = title
+        if description:
+            panel_embed.description = description
+        return await panel.edit(embed=panel_embed)
+
+    async def no_panel(self, ctx) -> discord.Message:
+        embed = discord.Embed(title="Reacton Roles Setup Cancelled",
+                              description=f"{ctx.author.mention}, the reaction roles setup was cancelled because the "
+                                          f"setup panel was deleted or could not be found",
+                              color=discord.Color.dark_red())
+        return await ctx.channel.send(embed=embed)
+
+    async def user_cancelled(self, ctx, panel: discord.Message) -> discord.Message:
+        embed = discord.Embed(title="Reaction Roles Setup Cancelled",
+                              description=f"{ctx.author.mention}, you have cancelled reaction roles setup",
+                              color=discord.Color.dark_red())
+        panel_message = await self.check_panel(panel)
+        if not panel_message:
+            return await ctx.channel.send(embed=embed, delete_after=10)
+        else:
+            return await panel_message.edit(embed=embed, delete_after=10)
+
+    async def timeout(self, ctx, timeout: int, panel: discord.Message) -> discord.Message:
+        embed = discord.Embed(title="Reaction Roles Setup Cancelled",
+                              description=f"{ctx.author.mention}, the reaction roles setup was canelled because you "
+                                          f"did not provide a valid action within {timeout} seconds",
+                              color=discord.Color.dark_red())
+        panel_message = await self.check_panel(panel)
+        if not panel_message:
+            return await ctx.channel.send(embed=embed, delete_after=10)
+        else:
+            return await panel_message.edit(embed=embed, delete_after=10)
+
     @commands.group(aliases=['rr'])
-    @commands.bot_has_permissions(administrator=True)
-    @commands.has_permissions(administrator=True)
+    @commands.bot_has_permissions(manage_roles=True, add_reactions=True)
+    @commands.has_permissions(manage_guild=True)
     async def reactionrole(self, ctx):
         await gcmds.invkDelete(gcmds, ctx)
-        return
+        if not ctx.invoked_subcommand:
+            embed = discord.Embed(title="ReactionRoles Help Menu",
+                                  description=f"All reaction roles commands can be accessed using "
+                                              f"`{gcmds.prefix(gcmds, ctx)}reactionrole [option]`. "
+                                              f"Below is a list of all the valid options",
+                                  color=discord.Color.blue())
+            embed.add_field(name="Create",
+                            value=f"Usage: `{gcmds.prefix(gcmds, ctx)}reactionrole create`"
+                                  f"Returns: Interactive reaction roles setup panel"
+                                  f"Aliases: `start` `make`")
+            return await ctx.channel.send(embed=embed)
 
-    @reactionrole.command(aliases=['c', 'make'])
-    async def create(self, ctx, *, fastArgs=None):
+    @reactionrole.command(aliases=['start', 'make'])
+    async def create(self, ctx):
 
-        def check(message, user):
-            if ctx.author == user and message.author == ctx.author and message.channel == ctx.channel:
+        panel_embed = discord.Embed(title="Reaction Role Setup Menu",
+                                    description=f"{ctx.author.mention}, welcome to MarwynnBot's reaction role setup "
+                                                f"menu. Just follow the prompts and you will have a working reaction "
+                                                f"roles panel!",
+                                    color=discord.Color.blue())
+        panel = await ctx.channel.send(embed=panel_embed)
+
+        await asyncio.sleep(5.0)
+
+        def from_user(message: discord.Message) -> bool:
+            if message.author == ctx.author and message.channel == ctx.channel:
                 return True
             else:
                 return False
 
-        if fastArgs is None:
-            rrGuide = discord.Embed(title="Reaction Roles Setup Menu",
-                                    description=f"{ctx.author.mention}, I will walk you through setup for reaction "
-                                                f"roles. Within 30 seconds, type `continue` to proceed or `cancel` to "
-                                                f"cancel",
-                                    color=discord.Color.blue())
-            setup_message = await ctx.channel.send(embed=rrGuide)
+        timeout = 30
 
-            while True:
-                try:
-                    response = commands.AutoShardedBot.wait_for(self.client, 'message', check=check, timeout=30)
-                except asyncio.TimeoutError:
-                    rrCancel = discord.Embed(title="Reaction Roles Setup Cancelled",
-                                             description=f"{ctx.author.mention}, setup was cancelled due to inactivity",
-                                             color=discord.Color.dark_red())
-                    await setup_message.edit(embed=rrCancel, delete_after=10)
-                    return
-                else:
-                    if response.content == "continue":
-                        try:
-                            await invalid_response.delete()
-                        except UnboundLocalError:
-                            pass
-                        break
-                    elif response.content == "cancel":
-                        rrCancel = discord.Embed(title="Reaction Roles Setup Cancelled",
-                                                 description=f"{ctx.author.mention}, you have cancelled setup",
-                                                 color=discord.Color.dark_red())
-                        await setup_message.edit(embed=rrCancel, delete_after=10)
-                        return
-                    else:
-                        invalid = discord.Embed(title="Invalid Response",
-                                                description=f"{ctx.author.mention}, please type `continue` to "
-                                                            f"continue or `cancel` to cancel",
-                                                color=discord.Color.dark_red())
-                        invalid_response = await ctx.channel.send(embed=invalid, delete_after=10)
-
-            rrGuide = discord.Embed(title="Reaction Roles Setup Menu",
-                                    description=f"{ctx.author.mention}, type the channel ID or tag the channel where "
-                                                f"the message should be sent in",
-                                    color=discord.Color.blue())
-            await setup_message.edit(embed=rrGuide)
-
+        # User will input the channel by tag
+        while True:
             try:
-                reponse = commands.AutoShardedBot.wait_for(self.client, 'message', check=check, timeout=30)
+                panel_message = await self.check_panel(panel)
+                if not panel_message:
+                    return self.no_panel(ctx)
+                await self.edit_panel(panel_embed, panel_message, title=None,
+                                      description=f"{ctx.author.mention}, please tag the channel you would like the "
+                                                  f"embed to be sent in (or type its ID)")
+                result = await commands.AutoShardedBot.wait_for(self.client, "message", check=from_user, timeout=timeout)
             except asyncio.TimeoutError:
-                rrCancel = discord.Embed(title="Reaction Roles Setup Cancelled",
-                                         description=f"{ctx.author.mention}, setup was cancelled due to inactivity",
-                                         color=discord.Color.dark_red())
-                await setup_message.edit(embed=rrCancel, delete_after=10)
-                return
-            else:
-                if "<" in response or ">" in response:
-                    channel_id = int(response[2:-1])
-                else:
-                    channel_id = int(response)
-                channel = commands.AutoShardedBot.get_channel(self.client, id=channel_id)
-                # TODO: Send Message to Channel ID HERE
-
-            rrGuide = discord.Embed(title="Reaction Roles Setup Menu",
-                                    description=f"{ctx.author.mention}, type the title of the embed you would like to "
-                                                f"send",
-                                    color=discord.Color.blue())
-            await setup_message.edit(embed=rrGuide)
-
-            try:
-                response = commands.AutoShardedBot.wait_for(self.client, 'message', check=check, timeout=30)
-            except asyncio.TimeoutError:
-                rrCancel = discord.Embed(title="Reaction Roles Setup Cancelled",
-                                         description=f"{ctx.author.mention}, setup was cancelled due to inactivity",
-                                         color=discord.Color.dark_red())
-                await setup_message.edit(embed=rrCancel, delete_after=10)
-                return
-            else:
-                title = response
-                # TODO: Use TITLE as the embed title
-
-            rrGuide = discord.Embed(title="Reaction Roles Setup Menu",
-                                    description=f"{ctx.author.mention}, type the message you would like to send",
-                                    color=discord.Color.blue())
-            await setup_message.edit(embed=rrGuide)
-
-            try:
-                response = commands.AutoShardedBot.wait_for(self.client, 'message', check=check, timeout=30)
-            except asyncio.TimeoutError:
-                rrCancel = discord.Embed(title="Reaction Roles Setup Cancelled",
-                                         description=f"{ctx.author.mention}, setup was cancelled due to inactivity",
-                                         color=discord.Color.dark_red())
-                await setup_message.edit(embed=rrCancel, delete_after=10)
-                return
-            else:
-                description = response
-                # TODO: Use DESCRIPTION as the embed description
-
-            rrGuide = discord.Embed(title="Reaction Roles Setup Menu",
-                                    description=f"{ctx.author.mention}, type the RGB value of the color you want the "
-                                                f"embed to be",
-                                    color=discord.Color.blue())
-            await setup_message.edit(embed=rrGuide)
-
-            while True:
-                try:
-                    response = commands.AutoShardedBot.wait_for(self.client, 'message', check=check, timeout=30)
-                except asyncio.TimeoutError:
-                    rrCancel = discord.Embed(title="Reaction Roles Setup Cancelled",
-                                             description=f"{ctx.author.mention}, setup was cancelled due to inactivity",
-                                             color=discord.Color.dark_red())
-                    await setup_message.edit(embed=rrCancel, delete_after=10)
-                    return
-                else:
-                    RGB = response.split()
-                    for item in RGB:
-                        if int(item) < 0 or int(item) > 255:
-                            invalid = discord.Embed(title="Invalid RGB Value",
-                                                    description="Please enter a valid RGB value",
-                                                    color=discord.Color.dark_red())
-                            invalid_response = ctx.channel.send(embed=invalid)
-                            continue
-                    try:
-                        await invalid_response.delete()
-                    except (UnboundLocalError, discord.NotFound):
-                        pass
-                    color = discord.Color.from_rgb(int(RGB[0]), int(RGB[1]), int(RGB[2]))
+                return await self.timeout(ctx, timeout, panel)
+            if not re.match(channel_tag_rx, result.content):
+                if re.match(channel_id_rx, result.content):
+                    channel_id = result.content
+                    await result.delete()
                     break
-                    # TODO: Use COLOR as the embed color
-
-            rolelist = []
-
-            rrGuide = discord.Embed(title="Reaction Roles Setup Menu",
-                                    description=f"{ctx.author.mention}, ping the role you would like to add, "
-                                                f"or type `done` to finish adding roles",
-                                    color=discord.Color.blue())
-            await setup_message.edit(embed=rrGuide)
-
-            while True:
-                try:
-                    response = commands.AutoShardedBot.wait_for(self.client, 'message', check=check, timeout=30)
-                except asyncio.TimeoutError:
-                    rrCancel = discord.Embed(title="Reaction Roles Setup Cancelled",
-                                             description=f"{ctx.author.mention}, setup was cancelled due to inactivity",
-                                             color=discord.Color.dark_red())
-                    await setup_message.edit(embed=rrCancel, delete_after=10)
-                    return
                 else:
-                    if response[0:4] == "<@&":
-                        rolelist.append(response)
-                    else:
-                        invalid = discord.Embed(title="Invalid Emoji",
-                                                description="Please enter a valid emoji",
-                                                color=discord.Color.dark_red())
-                        invalid_response = ctx.channel.send(embed=invalid)
-                        continue
-                    try:
-                        await invalid_response.delete()
-                    except (UnboundLocalError, discord.NotFound):
-                        pass
+                    continue
+            channel_id = result.content[2:20]
+            await result.delete()
+            break
 
-            emojilist = []
+        channel = await commands.AutoShardedBot.fetch_channel(self.client, channel_id)
 
-            rrGuide = discord.Embed(title="Reaction Roles Setup Menu",
-                                    description=f"{ctx.author.mention}, ping the emoji you would like to add, "
-                                                f"or type `done` to finish adding emojis",
-                                    color=discord.Color.blue())
-            await setup_message.edit(embed=rrGuide)
+        # User will input the embed title
+        try:
+            panel_message = await self.check_panel(panel)
+            if not panel_message:
+                return self.no_panel(ctx)
+            await self.edit_panel(panel_embed, panel_message, title=None,
+                                  description=f"{ctx.author.mention}, please enter the title of the embed that will "
+                                              f"be sent")
+            result = await commands.AutoShardedBot.wait_for(self.client, "message", check=from_user, timeout=timeout)
+        except asyncio.TimeoutError:
+            return await self.timeout(ctx, timeout, panel)
 
-            while True:
-                try:
-                    response = commands.AutoShardedBot.wait_for(self.client, 'message', check=check, timeout=30)
-                except asyncio.TimeoutError:
-                    rrCancel = discord.Embed(title="Reaction Roles Setup Cancelled",
-                                             description=f"{ctx.author.mention}, setup was cancelled due to inactivity",
-                                             color=discord.Color.dark_red())
-                    await setup_message.edit(embed=rrCancel, delete_after=10)
-                    return
-                else:
-                    if response[0] == ":" or response[0] == "<" or response[-1] == ":" or response[-1] == ">":
-                        emojilist.append(response)
-                        rrGuide.set_footer(text=f"Added emoji {response}")
-                        setup_message.edit(embed=rrGuide)
-                    elif response.content == "finish":
-                        break
-                    else:
-                        invalid = discord.Embed(title="Invalid Emoji",
-                                                description="Please enter a valid emoji",
-                                                color=discord.Color.dark_red())
-                        invalid_response = ctx.channel.send(embed=invalid)
-                        continue
-                    try:
-                        await invalid_response.delete()
-                    except (UnboundLocalError, discord.NotFound):
-                        pass
+        title = result.content
+        await result.delete()
 
-            rrGuide.set_footer(text=discord.Embed.Empty)
+        # User will input the embed description
+        try:
+            panel_message = await self.check_panel(panel)
+            if not panel_message:
+                return self.no_panel(ctx)
+            await self.edit_panel(panel_embed, panel_message, title=None,
+                                  description=f"{ctx.author.mention}, please enter the description of the embed that "
+                                              f"will be sent")
+            result = await commands.AutoShardedBot.wait_for(self.client, "message", check=from_user, timeout=timeout)
+        except asyncio.TimeoutError:
+            return await self.timeout(ctx, timeout, panel)
+
+        description = result.content
+        await result.delete()
+
+        # User will input the emoji, then appropriate role by tag
+        emoji_role_list = []
+        return
 
 
 def setup(client):
