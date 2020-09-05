ae0383a47f68e2db43b6452cfaa82e3ecb49fc8c
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-09-05 08:24:38 -0400

    fixed hijacking control panel

--- cogs/music.py
+++ cogs/music.py
@@ -39,12 +39,10 @@ class Music(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        if not reaction.message.guild:
+        if not reaction.message.guild or not user.voice or reaction.emoji not in reactions:
             return
         player = self.client.lavalink.player_manager.get(reaction.message.guild.id)
-        if reaction.emoji not in reactions:
-            return
-        if not user.bot and player:
+        if not user.bot and player and user.voice.channel.id == int(player.fetch('channel')):
             guild_id = reaction.message.guild.id
             message = self.info[str(guild_id)]['message']
             paused = self.info[str(guild_id)]['paused']
