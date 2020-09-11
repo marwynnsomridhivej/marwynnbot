e1b3dfab9d72a4725320feac1c0e24df162ecff4
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-09-10 20:05:54 -0400

    incremented version to v0.2.0-alpha.1

--- utils/globalcommands.py
+++ utils/globalcommands.py
@@ -38,7 +38,7 @@ default_env = ["YOUR_BOT_TOKEN",
 class GlobalCMDS:
 
     def __init__(self):
-        self.version = "v0.1.0-alpha.1"
+        self.version = "v0.2.0-alpha.1"
 
     def init_env(self):
         if not os.path.exists('.env'):
