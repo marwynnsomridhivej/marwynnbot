ed8f323c059a5901958071452a980a8d77d3178d
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-09-25 20:35:35 -0400

    tested print message.id

--- bot.py
+++ bot.py
@@ -114,6 +114,7 @@ class Bot(commands.AutoShardedBot):
                 for text_channel in guild.text_channels:
                     try:
                         message = await text_channel.fetch_message(message_id)
+                        print(message.id)
                         await con.execute(f"UPDATE base_rr SET jump_url={message.jump_url} WHERE message_id={message_id}")
                         print("Found")
                         break
