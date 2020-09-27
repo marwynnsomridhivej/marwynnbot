13f47dadb59f749d062263e478e3a4fd86e86cb7
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-09-27 17:51:50 -0400

    added ChannelEmoji enum

--- utils/enums.py
+++ utils/enums.py
@@ -1,11 +1,12 @@
 import enum
 
+
 class LogLevel(enum.IntEnum):
     NULL = 0
     BASIC = 1
     GUILD = 2
     HIDEF = 3
-    
+
     def __str__(self):
         return self.name.lower() if self.name != "GUILD" else "server"
 
@@ -13,9 +14,25 @@ class LogLevel(enum.IntEnum):
 class ConfirmReactions(enum.Enum):
     YES = '✅'
     NO = '🛑'
-    
+
     def __str__(self):
         return self.value
-    
+
     def __repr__(self):
         return f"<ConfirmationReactions name = {self.name}, value = {self.value}>"
+
+
+class ChannelEmoji(enum.Enum):
+    text = '📑'
+    voice = '🔊'
+    category = '📂'
+
+    @property
+    def emoji(self):
+        return self.value
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return f"<ChannelEmoji name = {self.name}, emoji = {self.value}"
