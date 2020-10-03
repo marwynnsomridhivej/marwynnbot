d0be577468525be4be0aeba9f04b58c80073daae
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-10-03 14:37:24 -0400

    added cards dicts for easy access

--- utils/cards.py
+++ utils/cards.py
@@ -78,3 +78,187 @@ playing_cards = {
         "Clubs": "<:AC:738097070233092208>",
     },
 }
+
+
+uno_thumbnail = {
+    "0": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903129520177162/0_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903614616600576/0_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904049163272382/0_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906035107627058/0_y.png",
+    },
+    "1": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903130770079834/1_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903618274033824/1_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904054024470598/1_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906040187060234/1_y.png",
+    },
+    "2": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903132061925386/2_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903621931466822/2_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904052950990929/2_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906044641280050/2_y.png",
+    },
+    "3": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903133479600129/3_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903625534373958/3_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904054850748536/3_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906048306970725/3_y.png",
+    }
+    "4": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903135027560498/4_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903629716226119/4_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904057128255568/4_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906053117837342/4_y.png",
+    },
+    "5": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903136394903672/5_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903633751146706/5_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904059330527242/5_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906056947368036/5_y.png",
+    },
+    "6": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903137812316170/6_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903638335520788/6_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904061461102652/6_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906061737132072/6_y.png",
+    },
+    "7": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903139213213696/7_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903642475167814/7_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904064107708456/7_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906066481021049/7_y.png",
+    },
+    "8": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903140677025873/8_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903646292115456/8_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904065747550218/8_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906070851354735/8_y.png",
+    },
+    "9": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903141801099364/9_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903650356265050/9_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739904072030879786/9_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906075280539748/9_y.png",
+    },
+    "block": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903209803612200/block_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903705465487410/block_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739905458776703049/block_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906086102106142/block_y.png",
+    },
+    "+2": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903214475804742/plus2_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903708946628689/plus2_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739905462836658197/plus2_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906090023518269/plus2_y.png",
+    },
+    "reverse": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739903217789305003/rev_r.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739903711450628256/rev_b.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739905465076547654/rev_g.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906094318485665/rev_y.png",
+    },
+    "+4": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739906507730321439/plus4.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739906507730321439/plus4.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739906507730321439/plus4.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906507730321439/plus4.png",
+        "black": "https://cdn.discordapp.com/attachments/734962101432615006/739906507730321439/plus4.png",
+    },
+    "wild": {
+        "red": "https://cdn.discordapp.com/attachments/734962101432615006/739906512671211661/wild.png",
+        "blue": "https://cdn.discordapp.com/attachments/734962101432615006/739906512671211661/wild.png",
+        "green": "https://cdn.discordapp.com/attachments/734962101432615006/739906512671211661/wild.png",
+        "yellow": "https://cdn.discordapp.com/attachments/734962101432615006/739906512671211661/wild.png",
+        "black": "https://cdn.discordapp.com/attachments/734962101432615006/739906512671211661/wild.png",
+    }
+}
+
+
+uno_cards = {
+    "0": {
+        "red": "<:0_r:739620092597370881>",
+        "blue": "<:0_b:739620128395755542>",
+        "green": "<:0_g:739620167956430958>",
+        "yellow": "<:0_y:739626572675416245>",
+    },
+    "1": {
+        "red": "<:1_r:739620093109207150>",
+        "blue": "<:1_b:739620128190234726>",
+        "green": "<:1_g:739620167700578412>",
+        "yellow": "<:1_y:739626572444598322>",
+    },
+    "2": {
+        "red": "<:2_r:739620093281042492>",
+        "blue": "<:2_b:739620128718585856>",
+        "green": "<:2_g:739620167717224470>",
+        "yellow": "<:2_y:739626572461375520>",
+    },
+    "3": {
+        "red": "<:3_r:739620094266572811>",
+        "blue": "<:3_b:739620128416727080>",
+        "green": "<:3_g:739620168199700591>",
+        "yellow": "<:3_y:739626572734267462>",
+    }
+    "4": {
+        "red": "<:4_r:739620093956194345>",
+        "blue": "<:4_b:739620128244760607>",
+        "green": "<:4_g:739620167897579571>",
+        "yellow": "<:4_y:739626572184813700>",
+    },
+    "5": {
+        "red": "<:5_r:739620094644191272>",
+        "blue": "<:5_b:739620128412401684>",
+        "green": "<:5_g:739620168212414464>",
+        "yellow": "<:5_y:739626572759302324>",
+    },
+    "6": {
+        "red": "<:6_r:739620094287544401>",
+        "blue": "<:6_b:739620128475447396>",
+        "green": "<:6_g:739620167725613098>",
+        "yellow": "<:6_y:739626572767821824>",
+    },
+    "7": {
+        "red": "<:7_r:739620094392402040>",
+        "blue": "<:7_b:739620128320127046>",
+        "green": "<:7_g:739620167541194803>",
+        "yellow": "<:7_y:739626572457443429>",
+    },
+    "8": {
+        "red": "<:8_r:739620094539333692>",
+        "blue": "<:8_b:739620128626442371>",
+        "green": "<:8_g:739620167692320801>",
+        "yellow": "<:8_y:739626572499124355>",
+    },
+    "9": {
+        "red": "<:9_r:739620094325424191>",
+        "blue": "<:9_b:739620128433373244>",
+        "green": "<:9_g:739620167918813375>",
+        "yellow": "<:9_y:739626572931268640>",
+    },
+    "block": {
+        "red": "<:block_r:739620094317035522>",
+        "blue": "<:block_b:739620128550813807>",
+        "green": "<:block_g:739620168019476561>",
+        "yellow": "<:block_y:739626573103366154>",
+    },
+    "+2": {
+        "red": "<:plus2_r:739620094476288060>",
+        "blue": "<:plus2_b:739620128014204960>",
+        "green": "<:plus2_g:739620167973077002>",
+        "yellow": "<:plus2_y:739626572692062338>",
+    },
+    "reverse": {
+        "red": "<:rev_r:739620094455578765>",
+        "blue": "<:rev_b:739620128123125893>",
+        "green": "<:rev_g:739620168048705656>",
+        "yellow": "<:rev_y:739626572616564797>",
+    },
+    "+4": {
+        "black": "<:plus4:739620056446795946>",
+    },
+    "wild": {
+        "black": "<:wild:739620056551653537>",
+    }
+}
