990d6d9038bbe865b7ad6dc66e303ca8026d1f2d
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-10-19 15:08:57 -0400

    added directly importing functions from modules

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,4 @@
+from utils._confirm import confirm
+from utils.customhandler import handle
 from utils.globalcommands import GlobalCMDS
-from utils.paginator import EmbedPaginator, FieldPaginator
+from utils.paginator import EmbedPaginator, FieldPaginator, SubcommandPaginator
